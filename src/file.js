d2d4106e9df07682b2b9f0a8087ca0a65ca8f823
samanthaduarte <samanthamdc97@gmail.com>
2020-06-08 14:30:15 -0600

    final

--- src/file.js
+++ src/file.js
@@ -4,39 +4,43 @@
  * informing them that they will qualify for free shipping if their 
  * order is more than $99
  */
-
-var promoMessage = "¡Envío gratis a la capital en compras de Q150 o más!";
-var minSubtotal = 150;
-var widgets;
-
-// Calculating subtotal and displaying the message
-var checkSubtotal = function(order) {
-  if (order) {
-    var subtotal = order.total - order.shipping;
-    if (subtotal > minSubtotal) {
-      alert(promoMessage);
-    }  
-  }
-}
-
-// Detecting whether we're on the cart page and get the cart info
-Ecwid.OnPageLoaded.add(function(page) {
-  widgets = Ecwid.getInitializedWidgets();
-
-  // if storefront widget is present on this page
-  for (i = 0; i < widgets.length ; i++) {
-    if (widgets[i] == "ProductBrowser") {
-      if ('CART' == page.type) {
-        Ecwid.Cart.calculateTotal(function(order) {
-          checkSubtotal(order);
-        });
-      }
+Ecwid.OnAPILoaded.add(function() {
+ 
+  var promoMessage = "¡Envío gratis a la capital en compras de Q150 o más!";
+  var minSubtotal = 150;
+  var widgets;
+  
+  // Calculating subtotal and displaying the message
+  var checkSubtotal = function(order) {
+    if (order) {
+      var subtotal = order.total - order.shipping;
+      if (subtotal > minSubtotal) {
+        alert(promoMessage);
+      }  
     }
   }
+  
+  // Detecting whether we're on the cart page and get the cart info
+  Ecwid.OnPageLoaded.add(function(page) {
+    widgets = Ecwid.getInitializedWidgets();
+  
+    // if storefront widget is present on this page
+    for (i = 0; i < widgets.length ; i++) {
+      if (widgets[i] == "ProductBrowser") {
+        if ('CART' == page.type) {
+          Ecwid.Cart.calculateTotal(function(order) {
+            checkSubtotal(order);
+          });
+        }
+      }
+    }
+  });
+  
+  // Get color value for the message and store it in color variable
+  var color = Ecwid.getAppPublicConfig(appId);
+  
+  // your code here
+  // ...
+
+Ecwid.refreshConfig();
 });
-
-// Get color value for the message and store it in color variable
-var color = Ecwid.getAppPublicConfig(appId);
-
-// your code here
-// ...
